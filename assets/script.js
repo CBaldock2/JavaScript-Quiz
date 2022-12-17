4a80a3b4f629a5ba29be5f20f4486a6a0e7dc1d7
Christopher Baldock <christophermbaldock@gmail.com>
2022-12-16 21:06:43 -0600

    Adds a timer and changes class and id names

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
 var timer = document.getElementById("timer");
 var start = document.getElementById("start");
-var highScore = document.querySelector("#highScore");
+var correct = document.getElementById("correct");
+var incorrect = document.getElementById("incorrect");
 var clear = document.querySelector("#clear");
 
-var answered = 0;
-
 //Timer Variables
-var timeLeft = 5;
+var timeLeft = 76;
 var penalty = 10;
 var holdInterval = 0;
+
+//Score variable
+var score = 0;
+
 
 //Timer
 start.addEventListener("click", function () {
@@ -25,21 +28,41 @@ start.addEventListener("click", function () {
 
 });
 
+//Keeps score
+function setScore() {
+    if(correct === true) {
+        score = score + 5;
+    }
+}
+
+//Subtracts 10 seconds if an answer is wrong
+function timeLoss() {
+    if(incorrect = true) {
+        timeLeft - penalty;
+    }
+}
+
+//I thought this would make the first question appear
+start.onclick = function () {
+    q1.style.display = "block";
+}
+
+//I thought this would make the game over page appear
+q1.onclick = function () {
+    gameOver.style.display = "block";
+}
+
+//Will add initials to the local.storage
+function saveScore() {
+    var initials = input;
+    localStorage.setItem("initials", initials);
+}
+
+//Will display the initials on the High score page
+var name = localStorage.getItem("initials")
+
+//Will clear the score board once i get a score saved
 clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
-
-var allScores = localStorage.getItem("allScores");
-allScores = JSON.parse(allScores);
-
-if (allScores !== null) {
-
-    for (var i = 0; i < allScores.length; i++) {
-        //Creates a list
-        var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initals + " " + allScores[i].score;
-        highScore.appendChild(createLi);
-
-    }
-}
